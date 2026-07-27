ca6d1177627107681a7de5da1720847b2f127ffb
Mz <>
2026-07-27 12:24:27 -0500

    add licensing included on scripts

--- extrapatches/pall.asm
+++ extrapatches/pall.asm
@@ -1,3 +1,4 @@
+
 org $0DB592
  db $80
 org $0DB918
--- extrapatches/storkslv.asm
+++ extrapatches/storkslv.asm
@@ -1,6 +1,5 @@
 ; Storks level selector patch
 ; Extracted from myzidya old rhtools asm.1
-;   https://raw.githubusercontent.com/Belthasaran/rhtools/a9efda3c7d4a8a4926043dbb94687fb25ec10c39/asm1.py
 ;
 ; Jump to level in storksa
 ; Parameter mapping JSON: INPUT: {"level_number": {"input": "glevelnum_s"}, "rom_file": {"input": "rom_file"}}
@@ -21,4 +20,19 @@ Main:
     RTS
     .etest:
     RTS
+
+;    Copyright (C) 2019-2026 Myzidya
+;
+;    This program is free software: you can redistribute it and/or modify
+;    it under the terms of the GNU Affero General Public License as published by
+;    the Free Software Foundation, either version 3 of the License, or
+;    (at your option) any later version.
+;
+;    This program is distributed in the hope that it will be useful,
+;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;    GNU Affero General Public License for more details.
+;
+;    You should have received a copy of the GNU Affero General Public License
+;    along with this program.  If not, see <https://gnu.org>.
 
